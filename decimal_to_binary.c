3e2f5361a6d38a44e27db912584370b008338e30
Aymeric RATINAUD <aratinau@e1r7p9.42.fr>
2015-06-22 15:08:36 +0200

    decimal to binary

---
+++ decimal_to_binary.c
@@ -0,0 +1,26 @@
+char *decimal_to_binary(int n)
+{
+	int c, d, count;
+	char *pointer;
+
+	count = 0;
+	pointer = (char*)malloc(32+1);
+
+	if ( pointer == NULL )
+		exit(EXIT_FAILURE);
+
+	for ( c = 31 ; c >= 0 ; c-- )
+	{
+		d = n >> c;
+
+		if ( d & 1 )
+			*(pointer+count) = 1 + '0';
+		else
+			*(pointer+count) = 0 + '0';
+
+		count++;
+	}
+	*(pointer+count) = '\0';
+
+	return  pointer;
+}
